357fc8e119f1c23132f8a19a3be2e197258db078
SamuelToh <samuelt@vrt.com.au>
2015-12-20 08:15:57 +1000

    refractor a bit

--- python/01-experiment-with-beautiful-soup-webscrapper/main.py
+++ python/01-experiment-with-beautiful-soup-webscrapper/main.py
@@ -18,11 +18,10 @@ events=soup.find('div', attrs={'class':'events'})
 
 for event_div in events.findAll('div'):
     try:
-        if(event_div.get("class")[0] == "monthLeft"):  month = event_div.text.strip()
-        if(event_div.get("class")[0] == "day"): day = event_div.text.strip()
-        if(event_div.get("class")[0] == "number"): number = event_div.text.strip()
-        #result.append("Event date -> " + month + " " + day + " " + number)
-        print (month + " " + day + " " + number)
+        if event_div.get("class")[0] == "monthLeft" :  month = event_div.text.strip()
+        if event_div.get("class")[0] == "day" : day = event_div.text.strip()
+        if event_div.get("class")[0] == "number" : number = event_div.text.strip()
+        print  month + " " + day + " " + number
     except:
         print "Cannot parse event div content:" + str(event_div)
         day = ""
